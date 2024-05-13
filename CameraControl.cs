b360be1791d1e48a86b99091c89626b5b3be70e6
c-dez <hdezcruzcarlos@gmail.com>
2024-05-13 17:19:16 -0600

    added rotate visuals

--- CameraControl.cs
+++ CameraControl.cs
@@ -5,13 +5,17 @@ using System;
 public partial class CameraControl : Node3D
 {
 	private CharacterBody3D player;
+	private  Node3D visuals;
 
-	[Export] private float mouseSens = 0.2f;
+
+	[Export] private float mouseSensX = 0.1f;
+	[Export] private float mouseSensY = 0.1f;
 
 	public override void _Ready()
 	{
 		Input.MouseMode = Input.MouseModeEnum.Captured;
 		player = GetNode<CharacterBody3D>("..");
+		visuals = GetNode<Node3D>("../Visuals");
 	}
 
     public override void _UnhandledInput(InputEvent @event)
@@ -22,15 +26,18 @@ public partial class CameraControl : Node3D
 			Vector2 mouseRelative = mouseEvent.Relative;
 
 			//rota player horizontal
-			player.RotateY(Mathf.DegToRad(-mouseRelative.X * mouseSens));
+			player.RotateY(Mathf.DegToRad(-mouseRelative.X * mouseSensX));
 			//rota camMount en vertical
-			RotateX(Mathf.DegToRad(-mouseRelative.Y * mouseSens));
+			RotateX(Mathf.DegToRad(-mouseRelative.Y * mouseSensY));
 
 			//clamp camMount vertical
 			float maxRotation = -60f;
 			float minRotation = 20;
 			RotationDegrees = new Vector3(
 				Mathf.Clamp(RotationDegrees.X, maxRotation, minRotation), 0, 0);
+
+			//rotate visuals 
+			visuals.RotateY(Mathf.DegToRad(-mouseRelative.X * mouseSensX));	
 		}
     }
 }
