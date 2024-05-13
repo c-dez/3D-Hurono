0df4a76db3d926a3de5f4e9653945355e56444f0
c-dez <hdezcruzcarlos@gmail.com>
2024-05-13 17:13:02 -0600

    added control the camera movement w mouse movement

--- CameraControl.cs
+++ CameraControl.cs
@@ -5,21 +5,32 @@ using System;
 public partial class CameraControl : Node3D
 {
 	private CharacterBody3D player;
-	private Node3D camMount;
+
+	[Export] private float mouseSens = 0.2f;
 
 	public override void _Ready()
 	{
 		Input.MouseMode = Input.MouseModeEnum.Captured;
 		player = GetNode<CharacterBody3D>("..");
-		camMount = GetNode<Node3D>("../CamMount");
 	}
 
     public override void _UnhandledInput(InputEvent @event)
     {
+		//controls the camera whit mouse movement
 		if (@event is InputEventMouseMotion mouseEvent)
 		{
 			Vector2 mouseRelative = mouseEvent.Relative;
-			GD.Print(camMount.Name);
+
+			//rota player horizontal
+			player.RotateY(Mathf.DegToRad(-mouseRelative.X * mouseSens));
+			//rota camMount en vertical
+			RotateX(Mathf.DegToRad(-mouseRelative.Y * mouseSens));
+
+			//clamp camMount vertical
+			float maxRotation = -60f;
+			float minRotation = 20;
+			RotationDegrees = new Vector3(
+				Mathf.Clamp(RotationDegrees.X, maxRotation, minRotation), 0, 0);
 		}
     }
 }
